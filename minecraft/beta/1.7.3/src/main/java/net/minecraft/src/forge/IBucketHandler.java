fbab718883538fcf8d0cc15ffb2aae21eef9ceac
Caroline Joy Bell <halotroop2288@proton.me>
2024-08-04 16:03:24 -0500

    made initial setup, implemented custom bucket
    https://sourceforge.net/p/minecraftforge/tickets/1/
    
    Co-authored-by: Space Toad <spacetoadcraft@gmail.com>

---
+++ minecraft/beta/1.7.3/src/main/java/net/minecraft/src/forge/IBucketHandler.java
@@ -0,0 +1,24 @@
+package net.minecraft.src.forge;
+
+import net.minecraft.src.*;
+import org.jetbrains.annotations.*;
+
+/**
+ * Event handler for filling buckets
+ *
+ * @author Space Toad
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface IBucketHandler {
+	/**
+	 * @param world the world to act on
+	 * @param x     the x position of the fluid
+	 * @param y     the y position of the fluid
+	 * @param z     the z position of the fluid
+	 * @return the new stack of the bucket containing the fluid
+	 * @author Space Toad
+	 * @since 1.0.0
+	 */
+	@Nullable ItemStack fillCustomBucket(@NotNull World world, int x, int y, int z);
+}
