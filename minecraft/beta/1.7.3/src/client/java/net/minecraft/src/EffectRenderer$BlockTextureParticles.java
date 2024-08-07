1f8004ec9b2ebdce795e0739ddc598aa7c432c38
Caroline Joy Bell <caroline@halotroop.com>
2024-08-07 18:34:26 -0500

    patched MC EffectRenderer to fulfill infinite sprite support
    Render needs to be changed a little to cover items also, though
    
    Co-authored-by: FlowerChild <flowerchild-fake@damnit.net>

---
+++ minecraft/beta/1.7.3/src/client/java/net/minecraft/src/EffectRenderer$BlockTextureParticles.java
@@ -0,0 +1,11 @@
+/*
+ * This software is provided under the terms of the Minecraft Forge Public License v1.1.
+ */
+package net.minecraft.src;
+
+import java.util.*;
+
+public class EffectRenderer$BlockTextureParticles {
+	public String texture;
+	public List<EntityFX> effects = new ArrayList<>();
+}
---
+++ minecraft/beta/1.7.3/src/client/java/net/minecraftforge/injection/ForgeEffectRenderer.java
@@ -0,0 +1,24 @@
+/*
+ * This software is provided under the terms of the Minecraft Forge Public License v1.1.
+ */
+package net.minecraftforge.injection;
+
+import net.minecraft.src.*;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * Adds public methods to {@link EffectRenderer} via Fabric Interface Injectors
+ *
+ * @author halotroop2288
+ */
+@ApiStatus.Internal
+public interface ForgeEffectRenderer {
+	/**
+	 * @author halotroop2288
+	 * @author FlowerChild
+	 * @author Space Toad
+	 */
+	default void addDigParticleEffect(EntityDiggingFX digEffect, Block block) {
+		throw new RuntimeException("Not implemented.");
+	}
+}
