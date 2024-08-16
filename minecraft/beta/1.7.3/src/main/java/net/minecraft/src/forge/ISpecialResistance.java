21de69d7e7d08bf5d2f06001823884de53c95b05
Caroline Joy Bell <caroline@halotroop.com>
2024-08-16 12:48:54 -0500

    Added explosion resistance hook.
    https://sourceforge.net/p/minecraftforge/tickets/12/
    
    Co-authored-by: Eloraam <eloraam@eloraam.com>

---
+++ minecraft/beta/1.7.3/src/main/java/net/minecraft/src/forge/ISpecialResistance.java
@@ -0,0 +1,32 @@
+/*
+ * This software is provided under the terms of the Minecraft Forge Public License v1.1.
+ */
+package net.minecraft.src.forge;
+
+import net.minecraft.src.*;
+
+/**
+ * This interface has to be implemented by a {@link Block}
+ * to give it a custom resistance to explosions.
+ *
+ * @author Eloraam
+ */
+public interface ISpecialResistance {
+	/**
+	 * @param world the world to act on
+	 * @param blockX the x position of this block
+	 * @param blockY the y position of this block
+	 * @param blockZ the z position of this block
+	 * @param sourceX the x position of the explosion source
+	 * @param sourceY the y position of the explosion source
+	 * @param sourceZ the z position of the explosion source
+	 * @param exploder the cause of the explosion (Examples: lit TNT, Creeper)
+	 * @return the resistance value to the explosion
+	 * @author Eloraam
+	 * @since 1.0.0
+	 */
+	float getSpecialExplosionResistance(World world,
+										int blockX, int blockY, int blockZ,
+										double sourceX, double sourceY, double sourceZ,
+										Entity exploder);
+}
