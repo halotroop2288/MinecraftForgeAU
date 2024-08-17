97b0a3c3660ddb7131b4f18ebac0352880468b24
Caroline Joy Bell <halotroop2288@proton.me>
2024-08-17 12:26:01 -0500

    Update buildscripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,39 @@
 plugins {
-    `kotlin-dsl`
-    alias(libs.plugins.unimined)
-    alias(libs.plugins.spotless)
+	`kotlin-dsl`
+	alias(libs.plugins.unimined)
+	alias(libs.plugins.spotless)
 }
 
 subprojects {
-    apply("plugin" to "xyz.wagyourtail.unimined")
-    apply("plugin" to "com.diffplug.spotless")
+	apply("plugin" to "xyz.wagyourtail.unimined")
+	apply("plugin" to "com.diffplug.spotless")
 
-    tasks.withType(JavaCompile::class.java).configureEach {
-        sourceCompatibility = "8"
-        targetCompatibility = "8"
-    }
+	java {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
 
-    spotless {
-        java {
-            licenseHeaderFile(File(rootProject.rootDir, "JAVA_HEADER"))
-        }
-    }
+		withJavadocJar()
+		withSourcesJar()
+	}
+
+	spotless {
+		java {
+			licenseHeaderFile(File(rootProject.rootDir, "JAVA_HEADER"))
+		}
+	}
+
+	tasks.withType(JavaCompile::class.java).configureEach {
+		sourceCompatibility = "8"
+		targetCompatibility = "8"
+	}
+
+	tasks.withType(ProcessResources::class.java).configureEach {
+		inputs.property ("version", project.version)
+
+		filesMatching("*.mod.json") {
+			expand ("version" to project.version)
+		}
+	}
+
+	unimined.useGlobalCache = false
 }
