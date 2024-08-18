c9bba46209e451bc8cfe2a91dee99ccf373d6faf
Caroline Joy Bell <halotroop2288@proton.me>
2024-08-18 14:10:24 -0500

    mirrored inner classes
    Removing inner classes was originally intended to fix Java 7 support, but is unnecessary now.
    
    Ignored:
    https://sourceforge.net/p/minecraftforge/tickets/14
    
    Co-authored-by: Space Toad <spacetoadcraft@gmail.com>

---
+++ minecraft/beta/1.7.3/src/main/java/net/minecraft/src/forge/Property.java
@@ -0,0 +1,19 @@
+/*
+ * This software is provided under the terms of the Minecraft Forge Public License v1.1.
+ */
+package net.minecraft.src.forge;
+
+/**
+ * @author Space Toad
+ * @since 1.0.1
+ * @deprecated use {@link Configuration.Property} instead.
+ */
+@Deprecated
+public class Property extends Configuration.Property {
+	/**
+	 * Default constructor.
+	 * @since 1.0.1
+	 */
+	public Property() {
+	}
+}
